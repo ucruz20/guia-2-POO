b2cc136957c8b89f09c926e413e1db30507cdbe7
CG21038-Ulises Cruz <cg21038@ues.edu.sv>
2023-08-22 23:41:40 -0600

    Inicializacion de proyecto

---
+++ src/main/java/com/mycompany/cg21038/poo/guian2/Cg210382023POOGuian2.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.cg21038.poo.guian2;
+
+
+/**
+ *
+ * @author ucruz
+ */
+public class Cg210382023POOGuian2 {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
