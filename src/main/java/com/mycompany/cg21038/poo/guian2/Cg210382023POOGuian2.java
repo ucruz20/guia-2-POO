84570453972b5ce51d5f8e4c5cf898f5f4e104b9
CG21038-Ulises Cruz <cg21038@ues.edu.sv>
2023-08-23 00:16:09 -0600

    adicon de funcionamiento en main

--- src/main/java/com/mycompany/cg21038/poo/guian2/Cg210382023POOGuian2.java
+++ src/main/java/com/mycompany/cg21038/poo/guian2/Cg210382023POOGuian2.java
@@ -1,6 +1,4 @@
-
 package com.mycompany.cg21038.poo.guian2;
-
 
 /**
  *
@@ -9,7 +7,11 @@ package com.mycompany.cg21038.poo.guian2;
 public class Cg210382023POOGuian2 {
 
     public static void main(String[] args) {
-
-        
+        Pet dog = new Pet("Perro", 6, 12.4, 1.4);
+        File files = new File(24, "Cancer terminal", "Juan Valdez");
+        files.consultaInfo();
+        files.dateInfo();
+        dog.PetInfo();
+        System.out.println("$" + files.valorConsulta(dog.getEdad()));
     }
 }
